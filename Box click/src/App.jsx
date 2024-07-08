7165818e5e88721f9995127b08055eb3240d3411
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-08 12:54:06 +0300

    tiral

---
+++ Box click/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [dark, setDark] = useState(false)
+
+  return (
+    <div className="grid">
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      
+    </div>
+  )
+}
+
+export default App
